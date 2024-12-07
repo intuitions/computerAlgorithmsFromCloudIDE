7aed2640b91fd0655a480ef7b4e66cc341e60fad
intuitions <tdcq16768@qq.com>
2024-12-07 05:23:29 +0000

    add bigIntegerMultiplication declare

--- main.cpp
+++ main.cpp
@@ -1,15 +1,35 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+//big integer multiplication
+
+string bigIntegerMultiplication(string &x,string &y)
+{
+    string multiResult = "9876543210";
+    multiResult = x + y;
+    return multiResult;
+}
+
+
+
 int main()
 {
+/*    
     unsigned int cycle = 0;
+    cout << "please input cycle:" <<endl;
     cin >> cycle;
     for(unsigned int i = 0;i < cycle ;i++)
     {
         cout << "hello world!" <<endl;
     }
+*/
+    string x = "12345";
+    string y = "67890";
+    string resultStr = "0";
+    resultStr = bigIntegerMultiplication(x,y);
+    cout << "resultStr:" << resultStr <<endl;
     return 0;
 }
 
