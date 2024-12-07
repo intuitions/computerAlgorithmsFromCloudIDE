468292731f2fc9dfb9380d58f91680b6fc9f1b0f
intuitions <tdcq16768@qq.com>
2024-12-07 05:05:22 +0000

    linked codespace

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    unsigned int cycle = 0;
+    cin >> cycle;
+    for(unsigned int i = 0;i < cycle ;i++)
+    {
+        cout << "hello world!" <<endl;
+    }
+    return 0;
+}
+
